b0de5f629f1acbbe58349e32b75ab4a5d40a069f
physics-sp <physics-sp@protonmail.com>
2021-03-11 16:57:44 -0300

    add og_type filed on entries
    also standarized the optput of get_retweeters_new

--- openglass/twitter.py
+++ openglass/twitter.py
@@ -5,10 +5,11 @@ import uuid
 
 
 class StreamListener(tweepy.StreamListener):
-    def __init__(self, callback, search_id, current_url):
+    def __init__(self, callback, search_id, current_url, type_param):
         self.callback = callback
         self.search_id = search_id
         self.current_url = current_url
+        self.type = type_param
         super().__init__()
 
     def on_status(self, status):
@@ -27,16 +28,21 @@ class Twitter:
         self.api = self.__authenticate(self.api_in_use)
         self.current_url = ''
         self.search_id = str(uuid.uuid4())
+        self.type = ''
 
     def get_retweeters(self, tweet_id):
         '''returns up to 100 user IDs that have retweeted the tweet'''
         self.current_url = '/statuses/retweeters/ids'
+        if self.type == '':
+            self.type = 'get_retweeters'
         return [standarize_entry(self, {'tweet_id': tweet_id, 'retweeter_id': retweeter}) for retweeter in self.__limit_handled(tweepy.Cursor(self.api.retweeters, id=tweet_id).items())]
 
     def get_retweeters_new(self, tweet_id, run_for):
         '''returns the new retweeters from a given tweet'''
         self.current_url = '/statuses/retweeters/ids'
+        self.type = 'get_retweeters_new'
         start_time = time.time()
+        retweeters_ids = []
         retweeters = []
         MINS_5 = 5 * 60
         HOURS_5 = 5 * 60 * 60
@@ -46,9 +52,10 @@ class Twitter:
                 new_retweeters = self.get_retweeters(tweet_id)
                 num_new_retweeters = 0
                 for new_retweeter in new_retweeters:
-                    if new_retweeter['retweeter_id'] not in retweeters:
+                    if new_retweeter['retweeter_id'] not in retweeters_ids:
                         num_new_retweeters += 1
-                        retweeters.append(new_retweeter['retweeter_id'])
+                        retweeters_ids.append(new_retweeter['retweeter_id'])
+                        retweeters.append(standarize_entry(self, {'tweet_id': tweet_id, 'retweeter_id': new_retweeter['retweeter_id']}))
 
                 # update how much we wait until the next request
                 # if we got 0 new retweets, double the time
@@ -68,34 +75,40 @@ class Twitter:
     def get_followers(self, user):
         '''returns the followers of a user, ordered from new to old'''
         self.current_url = '/followers/list'
+        self.type = 'get_followers'
         return [standarize_entry(self, follower._json) for follower in self.__limit_handled(tweepy.Cursor(self.api.followers, id=user).items())]
 
     def get_profile(self, user):
         '''returns the profile information of a user'''
         self.current_url = '/users/show'
+        self.type = 'get_profile'
         return standarize_entry(self, self.api.get_user(id=user)._json)
 
     def get_timeline(self, user):
         '''returns up to 3.200 of a user's most recent tweets'''
         self.current_url = '/statuses/user_timeline'
+        self.type = 'get_timeline'
         return [standarize_entry(self, tweet._json) for tweet in self.__limit_handled(tweepy.Cursor(self.api.user_timeline, id=user).items())]
 
     def get_timeline_new(self, users, callback):
         '''returns new tweets of a list of users'''
         self.current_url = '/statuses/user_timeline'
-        stream_listener = StreamListener(callback, self.search_id, self.current_url)
+        self.type = 'get_timeline_new'
+        stream_listener = StreamListener(callback, self.search_id, self.current_url, self.type)
         stream = tweepy.Stream(auth=self.api.auth, listener=stream_listener)
         stream.filter(follow=users.split(' '))
 
     def search(self, q):
         '''searches for already published tweets that match the search'''
         self.current_url = '/search/tweets'
+        self.type = 'search'
         return [standarize_entry(self, tweet._json) for tweet in self.__limit_handled(tweepy.Cursor(self.api.search, q=q).items())]
 
     def search_new(self, q, callback):
         '''returns new tweets that match the search'''
         self.current_url = '/search/tweets'
-        stream_listener = StreamListener(callback, self.search_id, self.current_url)
+        self.type = 'search_new'
+        stream_listener = StreamListener(callback, self.search_id, self.current_url, self.type)
         stream = tweepy.Stream(auth=self.api.auth, listener=stream_listener)
         stream.filter(track=q.split(' '))
 
@@ -154,5 +167,6 @@ def standarize_entry(obj, entry):
     entry['og_id'] = str(uuid.uuid4())
     entry['og_search_id'] = obj.search_id
     entry['og_timestamp'] = int(time.time())
-    entry['og_type'] = obj.current_url
+    entry['og_type'] = obj.type
+    entry['og_endpoint'] = obj.current_url
     return entry
