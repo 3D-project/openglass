3600117c579eab245f7a6c8cc553d8c961581247
physics-sp <physics-sp@protonmail.com>
2021-03-10 22:29:45 -0300

    add docstrings to twitter.py

--- openglass/twitter.py
+++ openglass/twitter.py
@@ -29,10 +29,12 @@ class Twitter:
         self.search_id = str(uuid.uuid4())
 
     def get_retweeters(self, tweet_id):
+        '''returns up to 100 user IDs that have retweeted the tweet'''
         self.current_url = '/statuses/retweeters/ids'
         return [standarize_entry(self, {'tweet_id': tweet_id, 'retweeter_id': retweeter}) for retweeter in self.__limit_handled(tweepy.Cursor(self.api.retweeters, id=tweet_id).items())]
 
     def get_retweeters_new(self, tweet_id, run_for):
+        '''returns the new retweeters from a given tweet'''
         self.current_url = '/statuses/retweeters/ids'
         start_time = time.time()
         retweeters = []
@@ -48,52 +50,64 @@ class Twitter:
                         num_new_retweeters += 1
                         retweeters.append(new_retweeter['retweeter_id'])
 
-                if run_for and time.time() - start_time > run_for:
-                    return retweeters
-
+                # update how much we wait until the next request
+                # if we got 0 new retweets, double the time
+                # if we got 100 new retweets, divide it by 2
                 change = -0.015 * num_new_retweeters + 2
                 sleeping_time *= change
-                if sleeping_time > HOURS_5:
-                    sleeping_time = HOURS_5
+                # don't wait for more than 5 hours
+                sleeping_time = min(sleeping_time, HOURS_5)
+
+                if run_for and time.time() + sleeping_time - start_time > run_for:
+                    return retweeters
+
                 time.sleep(sleeping_time)
             except KeyboardInterrupt:
                 return retweeters
 
     def get_followers(self, user):
+        '''returns the followers of a user, ordered from new to old'''
         self.current_url = '/followers/list'
         return [standarize_entry(self, follower._json) for follower in self.__limit_handled(tweepy.Cursor(self.api.followers, id=user).items())]
 
     def get_profile(self, user):
+        '''returns the profile information of a user'''
         self.current_url = '/users/show'
         return standarize_entry(self, self.api.get_user(id=user)._json)
 
     def get_timeline(self, user):
+        '''returns up to 3.200 of a user's most recent tweets'''
         self.current_url = '/statuses/user_timeline'
         return [standarize_entry(self, tweet._json) for tweet in self.__limit_handled(tweepy.Cursor(self.api.user_timeline, id=user).items())]
 
     def get_timeline_new(self, users, callback):
+        '''returns new tweets of a list of users'''
         self.current_url = '/statuses/user_timeline'
         stream_listener = StreamListener(callback, self.search_id, self.current_url)
         stream = tweepy.Stream(auth=self.api.auth, listener=stream_listener)
         stream.filter(follow=users.split(' '))
 
     def search(self, q):
+        '''searches for already published tweets that match the search'''
         self.current_url = '/search/tweets'
         return [standarize_entry(self, tweet._json) for tweet in self.__limit_handled(tweepy.Cursor(self.api.search, q=q).items())]
 
     def search_new(self, q, callback):
+        '''returns new tweets that match the search'''
         self.current_url = '/search/tweets'
         stream_listener = StreamListener(callback, self.search_id, self.current_url)
         stream = tweepy.Stream(auth=self.api.auth, listener=stream_listener)
         stream.filter(track=q.split(' '))
 
     def __authenticate(self, credentials):
+        '''authenticates to twitter with the given credentials'''
         auth = tweepy.OAuthHandler(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'])
         auth.set_access_token(credentials['ACCESS_KEY'], credentials['ACCESS_SECRET'])
         api = tweepy.API(auth)
         return api
 
     def __limit_handled(self, cursor):
+        '''used by the cursors, handles rate limit'''
         while True:
             try:
                 yield cursor.next()
@@ -108,6 +122,10 @@ class Twitter:
                     raise e
 
     def __handle_time_limit(self):
+        '''
+        changes the API key being used based on their expiration status
+        if all keys are expired, it waits the smallest amount of time possible
+        '''
         now = time.time()
         self.api_in_use['expired_at'][self.current_url] = now
 
@@ -124,6 +142,11 @@ class Twitter:
 
 
 def standarize_entry(obj, entry):
+    '''
+    remove unused entrys and add important fields
+    a unique id 'og_id', a search id 'og_search_id'
+    a timestamp 'og_timestamp' and a type 'og_type'
+    '''
     keys_to_delete = ['id_str', 'profile_background_color', 'profile_link_color', 'profile_sidebar_border_color', 'profile_sidebar_fill_color', 'profile_text_color', 'favorited', 'filter_level']
     for key_to_delete in keys_to_delete:
         if key_to_delete in entry:
