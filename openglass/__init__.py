02bce301da3ac013e22b120fb947abb69c6086ba
physics-sp <physics-sp@protonmail.com>
2021-03-19 15:18:34 -0300

    refactor parameter parsing

--- openglass/__init__.py
+++ openglass/__init__.py
@@ -162,52 +162,16 @@ def main(cwd=None):
 
     args = parser.parse_args()
 
-    version = bool(args.version)
-    settings = bool(args.settings)
-    csv = bool(args.csv)
-    jsonl = bool(args.jsonl)
-    domains = bool(args.domains)
-    twitter = bool(args.twitter)
-    timeline = bool(args.timeline)
-    q_timeline = args.timeline
-    timeline_new = bool(args.timeline_new)
-    q_timeline_new = args.timeline_new
-    search = bool(args.search)
-    q_search = args.search
-    search_new = bool(args.search_new)
-    q_search_new = args.search_new
-    profile = bool(args.profile)
-    q_profile = args.profile
-    followers = bool(args.followers)
-    q_followers = args.followers
-    friends = bool(args.friends)
-    q_friends = args.friends
-    retweeters = bool(args.retweeters)
-    q_retweeters = args.retweeters
-    retweeters_new = bool(args.retweeters_new)
-    q_retweeters_new = args.retweeters_new
-    watch = bool(args.watch)
-    q_watch = args.watch
-    telegram = bool(args.telegram)
-    channel_users = bool(args.channel_users)
-    q_channel_users = args.channel_users
-    channel_links = bool(args.channel_links)
-    channel_messages = bool(args.channel_messages)
-    run_for = bool(args.run_for)
-    q_run_for = args.run_for
-    q_channel_messages = args.channel_messages
-    config_filename = args.config
-
-    if version:
+    if args.version:
         print(
             "Openglass version {}".format(utility.version)
         )
         return
 
-    if settings:
-        if config_filename:
+    if args.settings:
+        if args.config:
             print(
-                "Openglass settings {}".format(utility.print_settings(config_filename))
+                "Openglass settings {}".format(utility.print_settings(args.config))
             )
         else:
             print(
@@ -215,211 +179,211 @@ def main(cwd=None):
             )
         return
 
-    if jsonl and csv:
+    if args.jsonl and args.csv:
         parser.print_help()
         return
 
-    if not telegram and not twitter:
+    if not args.telegram and not args.twitter:
         parser.print_help()
         return
 
-    if telegram and twitter:
+    if args.telegram and args.twitter:
         parser.print_help()
         return
 
     num_actions = 0
-    if twitter:
-        if search:
+    if args.twitter:
+        if args.search:
             num_actions += 1
-        if search_new:
+        if args.search_new:
             num_actions += 1
-        if timeline:
+        if args.timeline:
             num_actions += 1
-        if timeline_new:
+        if args.timeline_new:
             num_actions += 1
-        if profile:
+        if args.profile:
             num_actions += 1
-        if followers:
+        if args.followers:
             num_actions += 1
-        if friends:
+        if args.friends:
             num_actions += 1
-        if retweeters:
+        if args.retweeters:
             num_actions += 1
-        if retweeters_new:
+        if args.retweeters_new:
             num_actions += 1
-        if watch:
+        if args.watch:
             num_actions += 1
 
-    if telegram:
-        if channel_users:
+    if args.telegram:
+        if args.channel_users:
             num_actions += 1
-        if channel_messages:
+        if args.channel_messages:
             num_actions += 1
 
     if num_actions != 1:
         parser.print_help()
         return
 
-    if run_for and telegram:
+    if args.run_for and args.telegram:
         parser.print_help()
         return
 
-    if run_for:
-        if re.search(r'^\d+[smhd]$', q_run_for) is None:
+    if args.run_for:
+        if re.search(r'^\d+[smhd]$', args.run_for) is None:
             parser.print_help()
             return
-        amount = q_run_for[:-1]
-        span = q_run_for[-1]
+        amount = args.run_for[:-1]
+        span = args.run_for[-1]
         if span == 's':
-            q_run_for = int(amount)
+            args.run_for = int(amount)
         if span == 'm':
-            q_run_for = int(amount) * 60
+            args.run_for = int(amount) * 60
         if span == 'h':
-            q_run_for = int(amount) * 60 * 60
+            args.run_for = int(amount) * 60 * 60
         if span == 'd':
-            q_run_for = int(amount) * 24 * 60 * 60
+            args.run_for = int(amount) * 24 * 60 * 60
     else:
-        q_run_for = None
+        args.run_for = None
 
     # Re-load settings, if a custom config was passed in
-    if config_filename:
-        utility.load_settings(config_filename)
+    if args.config:
+        utility.load_settings(args.config)
     else:
         utility.load_settings()
 
     number_of_results = 0
     filename = ''
 
-    if twitter:
+    if args.twitter:
 
         def entry_handler(obj, entry):
             nonlocal number_of_results
             number_of_results += 1
-            if jsonl or csv:
+            if args.jsonl or args.csv:
                 print('Number of results: {}'.format(number_of_results), end='\r')
             entry = standarize_entry(obj, entry)
-            store_result(entry, csv, jsonl, filename, start_time)
-            if run_for and time.time() - start_time > q_run_for:
+            store_result(entry, args.csv, args.jsonl, filename, start_time)
+            if args.run_for and time.time() - start_time > args.run_for:
                 raise KeyboardInterrupt
 
-        if config_filename:
-            utility.load_settings(config_filename)
+        if args.config:
+            utility.load_settings(args.config)
         else:
             utility.load_settings()
 
         t = Twitter(utility.get_setting('twitter_apis'))
-        if search:
+        if args.search:
             print('Press Ctrl-C to exit')
-            filename = 'search_{}'.format(q_search.replace(' ', '_'))
+            filename = 'search_{}'.format(args.search.replace(' ', '_'))
             try:
-                t.search(q_search, entry_handler)
+                t.search(args.search, entry_handler)
             except KeyboardInterrupt:
                 pass
-        if search_new:
+        if args.search_new:
             print('Press Ctrl-C to exit')
-            filename = 'search_new_{}'.format(q_search_new.replace(' ', '_'))
+            filename = 'search_new_{}'.format(args.search_new.replace(' ', '_'))
             try:
-                t.search_new(q_search_new, entry_handler)
+                t.search_new(args.search_new, entry_handler)
             except KeyboardInterrupt:
                 pass
-        elif timeline:
+        elif args.timeline:
             print('Press Ctrl-C to exit')
-            filename = 'timeline_{}'.format(q_timeline.replace(' ', '_'))
+            filename = 'timeline_{}'.format(args.timeline.replace(' ', '_'))
             try:
-                t.get_timeline(q_timeline, entry_handler)
+                t.get_timeline(args.timeline, entry_handler)
             except KeyboardInterrupt:
                 pass
-        elif timeline_new:
+        elif args.timeline_new:
             print('Press Ctrl-C to exit')
-            filename = 'timeline_new_{}'.format(q_timeline_new.replace(' ', '_'))
+            filename = 'timeline_new_{}'.format(args.timeline_new.replace(' ', '_'))
             try:
-                t.get_timeline_new(q_timeline_new.split(' '), entry_handler)
+                t.get_timeline_new(args.timeline_new.split(' '), entry_handler)
             except KeyboardInterrupt:
                 pass
-        elif profile:
-            filename = 'profile_{}'.format(q_profile.replace(' ', '_'))
-            profile = t.get_profile(q_profile)
+        elif args.profile:
+            filename = 'profile_{}'.format(args.profile.replace(' ', '_'))
+            profile = t.get_profile(args.profile)
             profile = standarize_entry(t, profile)
             number_of_results += 1
-            store_result(profile, csv, jsonl, filename, start_time)
-        elif followers:
+            store_result(profile, args.csv, args.jsonl, filename, start_time)
+        elif args.followers:
             print('Press Ctrl-C to exit')
-            filename = 'followers_{}'.format(q_followers.replace(' ', '_'))
+            filename = 'followers_{}'.format(args.followers.replace(' ', '_'))
             try:
-                t.get_followers(q_followers, entry_handler)
+                t.get_followers(args.followers, entry_handler)
             except KeyboardInterrupt:
                 pass
-        elif friends:
+        elif args.friends:
             print('Press Ctrl-C to exit')
-            filename = 'friends_{}'.format(q_friends.replace(' ', '_'))
+            filename = 'friends_{}'.format(args.friends.replace(' ', '_'))
             try:
-                t.get_friends(q_friends, entry_handler)
+                t.get_friends(args.friends, entry_handler)
             except KeyboardInterrupt:
                 pass
-        elif retweeters:
+        elif args.retweeters:
             print('Press Ctrl-C to exit')
-            filename = 'retweeters_{}'.format(q_retweeters.replace(' ', '_'))
+            filename = 'retweeters_{}'.format(args.retweeters.replace(' ', '_'))
             try:
-                t.get_retweeters(q_retweeters, entry_handler)
+                t.get_retweeters(args.retweeters, entry_handler)
             except KeyboardInterrupt:
                 pass
-        elif retweeters_new:
+        elif args.retweeters_new:
             print('Press Ctrl-C to exit')
-            filename = 'retweeters_new_{}'.format(q_retweeters_new.replace(' ', '_'))
+            filename = 'retweeters_new_{}'.format(args.retweeters_new.replace(' ', '_'))
             try:
-                t.get_retweeters_new(q_retweeters_new.split(' '), entry_handler)
+                t.get_retweeters_new(args.retweeters_new.split(' '), entry_handler)
             except KeyboardInterrupt:
                 pass
-        elif watch:
+        elif args.watch:
             print('Press Ctrl-C to exit')
-            filename = 'watch_{}'.format(q_watch.replace(' ', '_'))
+            filename = 'watch_{}'.format(args.watch.replace(' ', '_'))
             try:
-                t.watch(q_watch.split(' '), entry_handler)
+                t.watch(args.watch.split(' '), entry_handler)
             except KeyboardInterrupt:
                 pass
 
-    if telegram:
-        if config_filename:
-            utility.load_settings(config_filename)
+    if args.telegram:
+        if args.config:
+            utility.load_settings(args.config)
         else:
             utility.load_settings()
 
         t = Telegram(utility.get_setting('telegram'))
 
-        if channel_users:
-            res = t.get_channel(q_channel_users)
-            filename = q_channel_users.replace(' ', '_')
-        elif channel_messages:
-            res = t.get_messages(q_channel_messages)
-            if search:
-                res = search_dict(res, q_search)
-            if domains:
+        if args.channel_users:
+            res = t.get_channel(args.channel_users)
+            filename = args.channel_users.replace(' ', '_')
+        elif args.channel_messages:
+            res = t.get_messages(args.channel_messages)
+            if args.search:
+                res = search_dict(res, args.search)
+            if args.domains:
                 res = t.parse_domains(res)
-            if channel_links:
+            if args.channel_links:
                 res = t.parse_channel_links(res)
-            filename = q_channel_messages.replace(' ', '_')
+            filename = args.channel_messages.replace(' ', '_')
         for entry in res:
-            store_result(entry, csv, jsonl, filename, start_time)
+            store_result(entry, args.csv, args.jsonl, filename, start_time)
 
     if number_of_results == 0:
         print('No results')
         return
 
-    if jsonl:
-        filename = "{}-{}.jsonl".format(filename, start_time)
-    elif csv:
-        filename = "{}-{}.csv".format(filename, start_time)
-    if jsonl or csv:
+    if args.jsonl:
+        filename = "{}_{}.jsonl".format(filename, start_time)
+    elif args.csv:
+        filename = "{}_{}.csv".format(filename, start_time)
+    if args.jsonl or args.csv:
         print('\n[+] created {}'.format(filename))
 
 
 def store_result(entry, csv, jsonl, filename, start_time):
     '''save the result in as a .csv, .jsonl or print as json'''
     if csv:
-        filename = "{}-{}.csv".format(filename, start_time)
+        filename = "{}_{}.csv".format(filename, start_time)
         save_as_csv(entry, filename)
     elif jsonl:
-        filename = "{}-{}.jsonl".format(filename, start_time)
+        filename = "{}_{}.jsonl".format(filename, start_time)
         save_as_jsonl(entry, filename)
     else:
         print(json.dumps(entry, indent=4, sort_keys=True))
